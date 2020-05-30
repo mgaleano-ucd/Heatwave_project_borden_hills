ea65f02a022fa6673ccafd764022b865677cdb15
mgaleno-ucd <mgaleano@ucd>
2020-05-30 12:21:58 -0700

    Gas exchange responses to leaf temperature

--- scripts/Plotting_A_vs_leaf_T_second_HW_without_leaky_pixel.R
+++ scripts/Plotting_A_vs_leaf_T_second_HW_without_leaky_pixel.R
@@ -83,22 +83,88 @@ str(diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4$treatment)
 ## Plot second HW RESPONSE
 A_vs_leafT_licor_second_HW_r_3_4 <-ggplot(diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4, aes(Tleaf, A, group = treatment, color = treatment)) +
   geom_point(alpha =0.4, size = 1.7 ) + 
-  stat_smooth(aes(y = A, group = treatment, color = treatment),method = "lm", formula = y ~ x + I(x^2), size = 1, alpha =0.6) +
+  stat_smooth(aes(y = A, group = treatment, color = treatment),method = "lm", formula = y ~ x + I(x^2), size = 1, alpha =0.4) +
+  stat_poly_eq(aes(label =  paste(stat(rr.label),stat(p.value.label),stat(eq.label), sep = "*\", \"*")),
+               formula =  y ~ x + I(x^2), parse = TRUE, size = 5, label.x.npc = "left", vjust =-0.1)+
   theme_classic() +
   scale_colour_viridis_d(direction = -1, begin = 0.05, end = 0.93, name = "Treatment", labels = c("Baseline (60% ET)", "2x baseline ET", "3x baseline ET"))+
   ylab ("Net photosynthesis (mol m⁻² s⁻¹)") +
-  ggtitle( " A vs leaf temperature second HW response (R3&4)") +
   theme(plot.title = element_text(hjust = 0.5, size = 18, face = "bold", family = "serif")) +
   xlab("LI-COR Leaf temperature (ºC)") +
-  theme(axis.title.y = element_text(size=14, face = "bold", family = "serif")) +
-  theme(axis.title.x = element_text(size=14,face = "bold", family = "serif")) +
+  theme(axis.title.y = element_text(size=21, family = "serif")) +
+  theme(axis.title.x = element_text(size=21, family = "serif")) +
   theme(legend.key.size = unit (0.5, "cm")) +
   theme(legend.key.width = unit(0.2,"cm"))+
   theme(legend.justification = "center")+
   theme(legend.position = "right") +
   theme(legend.title.align = 0)+
   scale_x_continuous(breaks=seq(25,50,5), limits = c (25,50)) +
-  scale_y_continuous(breaks=seq(0,30,5), limits = c (0,30))
+  scale_y_continuous(breaks=seq(0,30,5), limits = c (0,30)) +
+  theme(axis.text.x = element_text(size =18))+
+  theme(axis.text.y = element_text(size =18))
 
 ggsave(A_vs_leafT_licor_second_HW_r_3_4, filename = "figures/A_vs_leafT_licor_second_HW_r_3_4_without_leaky_pixel.pdf", device = cairo_pdf, 
        width = 8, height = 6)
+
+#####Confidence intervarls upper and lower####
+
+####change treatments 1,2 and 3 to see each fit ### 
+
+diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial<-diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4%>%
+  filter(treatment == 1)
+
+diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial%>%
+  tally()
+
+physio_respone_fit <- lm(A ~ poly(Tleaf, 2), data = diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial)
+
+
+summary(physio_respone_fit$fitted.values)
+summary(physio_respone_fit$coefficients)
+summary(physio_respone_fit$model)
+
+
+str(physio_respone_fit)
+
+physio_respone_fit <- predict(physio_respone_fit,diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial, interval = "confidence")
+
+summary(physio_respone_fit)
+
+diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial <- diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial %>% 
+  mutate(fit = physio_respone_fit[,"fit"],
+         lwr = physio_respone_fit[,"lwr"],
+         upr = physio_respone_fit[,"upr"])
+
+A_vs_leafT_licor_first_HW_r_3_4_trial_2 <-ggplot(diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial, aes(Tleaf, A)) +
+  geom_point(alpha =0.6, size = 1.7 ) + 
+  geom_smooth(aes(y = A),method = "lm", formula = y ~ x + I(x^2), size = 1, alpha =0.5, level= 0) +
+  stat_regline_equation(
+    aes(label =  paste(..eq.label.., ..rr.label.., sep = "~")),
+    formula = y ~ x + I(x^2), size = 3.5
+  ) +geom_ribbon(aes(ymin =lwr, ymax =upr), alpha=0.3) +
+  theme_classic()
+
+
+physio_respone_fit <- lm(A ~ poly(Tleaf, 2), data = diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial)
+
+
+summary(physio_respone_fit)
+physio_respone_fit_pred <- predict(physio_respone_fit,diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial, interval = "prediction")
+
+diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial <- diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial %>% 
+  mutate(fit = physio_respone_fit_pred[,"fit"],
+         lwr = physio_respone_fit_pred[,"lwr"],
+         upr = physio_respone_fit_pred[,"upr"])
+
+A_vs_leafT_licor_first_HW_r_3_4_trial_2 <-ggplot(diurnals_2019_A_vs_leafT_licor_second_HW_r_3_4_trial, aes(Tleaf, A)) +
+  geom_point(alpha =0.6, size = 1.7 ) + 
+  geom_smooth(aes(y = A),method = "lm", formula = y ~ x + I(x^2), size = 1, alpha =0.5, level= 0) +
+  stat_regline_equation(
+    aes(label =  paste(..eq.label.., ..rr.label.., sep = "~")),
+    formula = y ~ x + I(x^2), size = 3.5
+  ) +geom_ribbon(aes(ymin =lwr, ymax =upr), alpha=0.3) +
+  theme_classic()
+
+
+
+
--- scripts/Plotting_gsw_vs_leaf_T_second_HW_without_leaky_pixel.R
+++ scripts/Plotting_gsw_vs_leaf_T_second_HW_without_leaky_pixel.R
@@ -81,24 +81,65 @@ str(diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4$treatment)
 ## Plot second HW RESPONSE
 gsw_vs_leafT_licor_second_HW_r_3_4 <-ggplot(diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4, aes(Tleaf, gsw, group = treatment, color = treatment)) +
   geom_point(alpha =0.4, size = 1.7 ) + 
-  stat_smooth(aes(y = gsw, group = treatment, color = treatment),method = "lm", formula = y ~ x + I(x^2), size = 1, alpha =0.6) +
+  stat_smooth(aes(y = gsw, group = treatment, color = treatment),method = "lm", formula = y ~ x + I(x^2), size = 1, alpha =0.4) +
+  stat_poly_eq(aes(label =  paste(stat(rr.label),stat(p.value.label), stat(eq.label), sep = "*\", \"*")),
+               formula =  y ~ x + I(x^2), parse = TRUE, size = 5, label.x.npc = "left", vjust =-0.1)+
   theme_classic() +
   scale_colour_viridis_d(direction = -1, begin = 0.05, end = 0.93, name = "Treatment", labels = c("Baseline (60% ET)", "2x baseline ET", "3x baseline ET"))+
   ylab ("Stomatal conductance ( mol m⁻² s⁻¹)") +
-  ggtitle( " gsw vs leaf temperature second HW response (R3&4)") +
   theme(plot.title = element_text(hjust = 0.5, size = 18, face = "bold", family = "serif")) +
   xlab("LI-COR Leaf temperature (ºC)") +
-  theme(axis.title.y = element_text(size=14, face = "bold", family = "serif")) +
-  theme(axis.title.x = element_text(size=14,face = "bold", family = "serif")) +
+  theme(axis.title.y = element_text(size=21, family = "serif")) +
+  theme(axis.title.x = element_text(size=21, family = "serif")) +
   theme(legend.key.size = unit (0.5, "cm")) +
   theme(legend.key.width = unit(0.2,"cm"))+
   theme(legend.justification = "center")+
   theme(legend.position = "right") +
   theme(legend.title.align = 0)+
   scale_x_continuous(breaks=seq(25,50,5), limits = c (25,50)) +
-  scale_y_continuous(breaks=seq(0,0.7,0.1), limits = c (0,0.78))
+  scale_y_continuous(breaks=seq(0,0.7,0.1), limits = c (0,0.78))+
+  theme(axis.text.x = element_text(size =18))+
+  theme(axis.text.y = element_text(size =18))
 
 ggsave(gsw_vs_leafT_licor_second_HW_r_3_4, filename = "figures/gsw_vs_leafT_licor_second_HW_r_3_4_without_leaky_pixel.pdf", device = cairo_pdf, 
        width = 8, height = 6)
 
+####Plotting all together####
 
+library(cowplot)
+
+panel_plot_A_and_gsw_vs_tleaf_both_HWS <- plot_grid (A_vs_leafT_licor_first_HW_r_3_4,A_vs_leafT_licor_second_HW_r_3_4,gsw_vs_leafT_licor_first_HW_r_3_4,gsw_vs_leafT_licor_second_HW_r_3_4, ncol=2, nrow = 2)
+
+ggsave(panel_plot_A_and_gsw_vs_tleaf_both_HWS , filename = "figures/panel_plot_A_and_gsw_vs_tleaf_both_HWS.pdf", device = cairo_pdf, width = 16, height = 11)
+
+
+#####Confidence intervarls upper and lower####
+
+####change treatments 1,2 and 3 to see each fit ### 
+
+
+diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4_trial<-diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4%>%
+  filter(treatment == 1)
+
+diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4_trial%>%
+  tally()
+
+physio_respone_fit <- lm(gsw ~ poly(Tleaf, 2), data = diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4_trial)
+
+
+summary(physio_respone_fit$fitted.values)
+summary(physio_respone_fit$coefficients)
+summary(physio_respone_fit$model)
+
+
+str(physio_respone_fit)
+
+physio_respone_fit <- predict(physio_respone_fit,diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4_trial, interval = "confidence")
+
+summary(physio_respone_fit)
+
+
+diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4_trial <- diurnals_2019_gsw_vs_leafT_licor_second_HW_r_3_4_triall %>% 
+  mutate(fit = physio_respone_fit[,"fit"],
+         lwr = physio_respone_fit[,"lwr"],
+         upr = physio_respone_fit[,"upr"])
